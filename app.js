5f785f3507554dabef525bddf430a808711d802a
Andre Pato <anpato1994@gmail.com>
2019-04-01 15:12:30 -0400

    created canvas and added sprites

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-let canvas = document.querySelector('.game-area');
-let player = document.querySelector('.player');
-
-const board = [
-    {x:0,y:0},
-    
-]
 
 
-document.querySelector('body').addEventListener('keydown', event=> {
-player.style.top = '100px'
-    
-})
+let player
+let playerImage
+let enemy
+let enemyImage
+let enemy2
+let enemy2Image
+
+function preload(){
+    playerImage = loadImage('./icons/player.png');
+    enemyImage = loadImage('./icons/red.png');
+    enemy2Image = loadImage('icons/leader.png');
+}
+
+function setup() {
+    createCanvas(600,600);
+    player = createSprite(width / 2, height - 25, 50, 50)
+    player.addImage(playerImage)
+    enemy = createSprite(width / 2, height/ 50,50);
+    enemy.addImage(enemyImage);
+    enemy2 = createSprite(width / 2, height/ 50,50);
+    enemy2.addImage(enemy2Image);
+    //  player.addImage(img);
+}
+
+function draw() {
+    background(0,0,0, 0)
+    drawSprites();
+}
