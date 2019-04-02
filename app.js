c77ddffb536f15c3009c1baa2d132da80813059b
Andre Pato <anpato1994@gmail.com>
2019-04-01 20:50:33 -0400

    added audio and random logic

--- app.js
+++ app.js
@@ -1,63 +1,69 @@
+
+
+
+//P5 Methods
 let player
 let playerImage
 let enemy
-let enemyImage
 let enemy2
-let enemy2Image
-let enemy3
-let enemy3Image
-let enemy4
-let enemy4Image
+
+let score =0;
 
 function preload() {
     playerImage = loadImage('./icons/player.png');
-    enemyImage = loadImage('./icons/red.png');
-    enemy2Image = loadImage('icons/teal.png');
-    enemy3Image = loadImage('icons/purple.png');
-    enemy4Image = loadImage('icons/leader.png');
 }
 
 function setup() {
     createCanvas(600, 600);
+    isGameOver = false
     player = createSprite(width / 2, height - 30, 20, 20)
     player.addImage(playerImage)
-    enemy = createSprite(20, 10, 10);
-    enemy.addImage(enemyImage);
-    enemy2 = createSprite(width / 2, height - 30, 0, 0);
-    enemy2.addImage(enemy2Image);
-    enemy3 = createSprite(width / 2, height - 30, 0, 0);
-    enemy3.addImage(enemy3Image);
-    enemy4 = createSprite(width / 2, height - 30, 0, 0);
-    enemy4.addImage(enemy4Image);
+    enemy = createSprite(random(width), random(height),random(10,300), random(50-200));
+    enemy2 = createSprite(random(width), random(height),random(10,100), random(1-60));
+    enemy3 = createSprite(random(width), random(height),random(10,250), random(5-100));
     frameRate(60);
-}4
-function draw() {
-    background(255, 255, 255)
-    if (keyDown(RIGHT_ARROW) && player.position.x < width - 25) {
-        player.position.x = player.position.x + 7
+}
+    function draw() {
+        // background(255, 255, 255)
+        clear()
+        //random enemy sizes
+
+        if (keyDown(RIGHT_ARROW) && player.position.x < width - 25) {
+            player.position.x = player.position.x + 7
+        }
+        if (keyDown(LEFT_ARROW) && player.position.x > 25) {
+            player.position.x = player.position.x - 7
+        }
+        frameRate(60);
+        enemy.position.y = enemy.position.y +random(10);
+        enemy2.position.y = enemy2.position.y + 5;
+        enemy3.position.y = enemy3.position.y + 8;
+        if (enemy.position.y > height + 100) {
+            enemy.position.y = 0
+            enemy.position.x = random(5, width - 5)
+        }
+        if (enemy2.position.y > height + 100) {
+            enemy2.position.y = 0
+            enemy2.position.x = random(5, width - 5)
+        }
+        if (enemy3.position.y > height + 100) {
+            enemy3.position.y = 0
+            enemy3.position.x = random(5, width - 5)
+        }
+    
+        if(isGameOver){
+            gameOver()
+        } else{
+        if(enemy.overlap(player)||enemy2.overlap(player)||enemy3.overlap(player)){
+            isGameOver = true
+        }}
+        drawSprites();
     }
-    if (keyDown(LEFT_ARROW) && player.position.x > 25) {
-        player.position.x = player.position.x - 7
-    }
-    enemy.position.y = enemy.position.y + 5;
-    enemy2.position.y = enemy2.position.y + 5;
-    enemy3.position.y = enemy3.position.y + 5;
-    enemy4.position.y = enemy4.position.y + 5;
-    if (enemy.position.y > height + 20) {
-        enemy.position.y = 0
-        enemy.position.x = random(8, width - 5)
-    }
-    if (enemy2.position.y > height + 30) {
-        enemy2.position.y = 0
-        enemy2.position.x = random(8, width - 5)
-    }
-    if (enemy3.position.y > height + 20) {
-        enemy3.position.y = 0
-        enemy3.position.x = random(8, width - 5)
-    }
-    if (enemy4.position.y > height + 20) {
-        enemy4.position.y = 0
-        enemy4.position.x = random(8, width - 5)
-    }
-    drawSprites();
+
+
+function gameOver(){
+    background(255)
+    textAlign(CENTER)
+    fill('white')
+    text('Game Over')
 }
