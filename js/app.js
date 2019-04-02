42d258f5560d48237c2b32dbcfa96fb4be3e98fc
Andre Pato <anpato1994@gmail.com>
2019-04-02 11:15:20 -0400

    added game start function and began adding logic for increasing score

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-
-
-
+let scoreBoard = document.querySelector('.scoreboard');
+// console.log(scoreBoard);
 //P5 Methods
 let player
 let playerImage
 let enemy
 let enemy2
 
-let score =0;
+let score = 0;
 
 function preload() {
     playerImage = loadImage('../icons/player.png');
@@ -15,51 +14,55 @@ function preload() {
 
 function setup() {
     createCanvas(600, 600);
+    frameRate(60);
     isGameOver = false
     player = createSprite(width / 2, height - 30, 20, 20)
     player.addImage(playerImage)
-    enemy = createSprite(random(width), random(height),random(10,300), random(50-200));
-    enemy2 = createSprite(random(width), random(height),random(10,100), random(1-60));
-    enemy3 = createSprite(random(width), random(height),random(10,250), random(5-100));
-    frameRate(60);
-
+    enemy = createSprite(width / 20, 20, height / 2, 10);
+    enemy2 = createSprite(width / 10, 5, height / 2, 5);
+    enemy3 = createSprite(width / 5, 5, height / 6, 2);
+    console.log(enemy3);
+    console.log(player);
 }
-    function draw() {
-        background(33, 33, 33)
-        clear()
-        //random enemy sizes
 
-        if (keyDown(RIGHT_ARROW) && player.position.x < width - 25) {
-            player.position.x = player.position.x + 7
-        }
-        if (keyDown(LEFT_ARROW) && player.position.x > 25) {
-            player.position.x = player.position.x - 7
-        }
-        enemy.position.y = enemy.position.y + 7;
-        enemy2.position.y = enemy2.position.y + 5;
-        enemy3.position.y = enemy3.position.y + 8;
-        if (enemy.position.y > height + 100 && enemy2.position.y > height + 100 && enemy3.position.y > 100) {
-            enemy.position.y = 0
-            enemy.position.x = random(5, width - 5)
-            enemy2.position.y = 0
-            enemy2.position.x = random(5, width - 5)
-            enemy3.position.y = 0
-            enemy3.position.x = random(5, width - 5)
-        }
-    
-        if(isGameOver){
-            gameOver()
-        }
-        if(enemy.overlap(player)||enemy2.overlap(player)||enemy3.overlap(player)){
-            isGameOver = true
-        }
-        drawSprites();
+function draw() {
+
+    background(33, 33, 33)
+    clear()
+    if (keyDown(RIGHT_ARROW) && player.position.x < width - 30) {
+        player.position.x = player.position.x + 10
     }
-
-
-function gameOver(){
-    background(255)
-    textAlign(CENTER)
-    fill('white')
-    text('Game Over')
+    if (keyDown(LEFT_ARROW) && player.position.x > 30) {
+        player.position.x = player.position.x - 10
+    }
+    enemy.position.y = enemy.position.y + 6;
+    enemy2.position.y = enemy2.position.y + 4;
+    enemy3.position.y = enemy3.position.y + 8;
+    if (enemy.position.y > height + 100 && enemy2.position.y > height + 100 && enemy3.position.y > 100) {
+        enemy.position.y = 0
+        enemy.position.x = random(10, width - 5)
+        enemy2.position.y = 0
+        enemy2.position.x = random(10, width - 5)
+        enemy3.position.y = 0
+        enemy3.position.x = random(10, width - 5)
+    }
+    if (isGameOver) {
+        gameOver()
+    }
+    if (enemy.overlap(player) || enemy2.overlap(player) || enemy3.overlap(player)) {
+        isGameOver = true
+    }
+    drawSprites();
 }
+
+function gameStart() {
+    started = true
+
+}
+
+function gameOver() {
+
+}
+
+
+
