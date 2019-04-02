05245518d7116837d21a17202839122972550f2e
Andre Pato <anpato1994@gmail.com>
2019-04-02 13:21:32 -0400

    added game over function that displays score and has reset button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 let scoreBoard = document.querySelector('.scoreboard');
+let modal = document.querySelector('.modal');
+let btn = document.createElement('button');
+let ptag = document.createElement('p');
 // console.log(scoreBoard);
 //P5 Methods
 let player
 let playerImage
 let enemy
 let enemy2
-
+let enemy3;
+let enemy4;
 let score = 0;
+let isGameOver;
 
 function preload() {
     playerImage = loadImage('../icons/player.png');
@@ -14,21 +19,22 @@ function preload() {
 
 function setup() {
     createCanvas(600, 600);
-    frameRate(60);
+    frameRate(120);
     isGameOver = false
     player = createSprite(width / 2, height - 30, 20, 20)
     player.addImage(playerImage)
     enemy = createSprite(width / 20, 20, height / 2, 10);
     enemy2 = createSprite(width / 10, 5, height / 2, 5);
     enemy3 = createSprite(width / 5, 5, height / 6, 2);
-    console.log(enemy3);
-    console.log(player);
+    
 }
 
 function draw() {
+    // setInterval(createNewSprites(), 300000);
+    background(33,33,33)
+    // clear();
 
-    background(33, 33, 33)
-    clear()
+    
     if (keyDown(RIGHT_ARROW) && player.position.x < width - 30) {
         player.position.x = player.position.x + 10
     }
@@ -38,7 +44,10 @@ function draw() {
     enemy.position.y = enemy.position.y + 6;
     enemy2.position.y = enemy2.position.y + 4;
     enemy3.position.y = enemy3.position.y + 8;
+    
     if (enemy.position.y > height + 100 && enemy2.position.y > height + 100 && enemy3.position.y > 100) {
+        incrementScore();
+        // console.log(score);
         enemy.position.y = 0
         enemy.position.x = random(10, width - 5)
         enemy2.position.y = 0
@@ -48,11 +57,13 @@ function draw() {
     }
     if (isGameOver) {
         gameOver()
-    }
+    }else {
     if (enemy.overlap(player) || enemy2.overlap(player) || enemy3.overlap(player)) {
         isGameOver = true
-    }
+    }}
+    
     drawSprites();
+    
 }
 
 function gameStart() {
@@ -61,8 +72,29 @@ function gameStart() {
 }
 
 function gameOver() {
+    modal.classList.add('active');
 
+    modal.appendChild(ptag);
+    modal.appendChild(btn);
+
+    score.pause    
+    ptag.innerText = score;
+    btn.innerText = 'Restart'
+    
+    background(0);
+    noLoop()
 }
 
+function incrementScore() {
+    score += 50
+    scoreBoard.innerText = score;
+}
 
+function createNewSprites(){
+    newSprite = setInterval(createSprite(random(width),random(height)),3000);
 
+    console.log(newSprite);
+    
+}
+
+btn.addEventListener('click', setup)
