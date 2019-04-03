d54ddee51ad82ca7a2ad63a9cbd9dc4c01adf2ac
Andre Pato <anpato1994@gmail.com>
2019-04-03 18:43:49 -0400

    working array movement and random positioning

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ let score = 0;
 let isGameOver;
 let time = 0;
 
-let wallArr = [];
+let wallsArr = [];
 let obstacle;
 let index;
 
@@ -26,44 +26,42 @@ function setup() {
     frameRate(120);
     player = createSprite(width / 2, height - 30, 20, 20)
     player.addImage(playerImage)
-    for (let i = 0; i < 10; i++) {
-        obstacle = createSprite(width / 10, 5, height / 2, 5);
-        wallArr.push(obstacle);
-        index = wallArr[i]
-        console.log(index);
+    for (let i = 0; i < 3; i++) {
+        wallsArr.push(new Walls())
     }
+    setInterval(() => {
+        for (let i = 0; i < 3; i++) {
+            wallsArr.push(new Walls())
+        }
+    }, 6000);
 }
 
 
 function draw() {
-    // timeCounter()
-    // console.log(time)
-
+    background(33, 33, 33);
+    drawSprites();
+    for (let i = 0; i < wallsArr.length; i++) {
+        obstacle = wallsArr[i]
+        wallsArr[i].move();
+        wallsArr[i].display();
+        // wallsArr[i].collision()
+        // console.log(wallsArr[i]);
+        // console.log(obstacle);
+    }
     if (isGameOver) {
         gameOver()
     } else {
-        if (index.overlap(player)) {
-            // isGameOver = true;
+        if (wallsArr.overlap(player)) {
+            isGameOver = true;
         }
-
         if (keyDown(RIGHT_ARROW) && player.position.x < width - 30) {
             player.position.x = player.position.x + 10
         }
         if (keyDown(LEFT_ARROW) && player.position.x > 30) {
             player.position.x = player.position.x - 10
         }
-        index.position.y = index.position.y + 6;
-
-        if (index.position.y > height + 100) {
-            incrementScore();
-            console.log(score);
-            index.position.y = 0;
-            index.position.x = random(10, width - 5);
-
-        }
+        // if (obstacle.position.y > height + 100) {}
     }
-    background(33, 33, 33);
-    drawSprites();
 };
 
 function restart() {
@@ -92,7 +90,31 @@ function incrementScore() {
     score += 50
     scoreBoard.innerText = score;
 }
+class Walls {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.w = random(width / 20, 180)
+        this.h = random(height / 50);
+        this.speed = 1;
+    }
 
-function timeCounter() {
-    return setInterval(time += 1, 3000);
+    move() {
+        this.x += random(-this.speed, this.speed);
+        this.y += random(+this.speed, this.speed);
+        if (this.y > height + 100) {
+            this.y = 0;
+            // incrementScore();
+            // console.log(score);
+        }
+    }
+
+    display() {
+        rect(this.x, this.y, this.w, this.h);
+    }
+    // collision() {
+    //     if (Walls.overlap(player)){
+    //         isGameOver=true;
+    //     }
+    // }
 }
