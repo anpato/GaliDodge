c11d2fd5a9f4413c863623d9421d53c18fcb9e3c
Andre Pato <anpato1994@gmail.com>
2019-04-03 12:51:17 -0400

    added time counter

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ let modal = document.querySelector('.modal');
 let btn = document.createElement('button');
 let ptag = document.createElement('p');
 
-// console.log(scoreBoard);
 //P5 Methods
 let player
 let playerImage
@@ -11,8 +10,12 @@ let enemy
 let enemy2
 let enemy3;
 let enemy4;
+let enemy5;
+let enemy6;
 let score = 0;
 let isGameOver;
+let time = 0;
+
 
 function preload() {
     playerImage = loadImage('../icons/player.png');
@@ -27,19 +30,23 @@ function setup() {
     enemy = createSprite(width / 20, 20, height / 2, 10);
     enemy2 = createSprite(width / 10, 5, height / 2, 5);
     enemy3 = createSprite(width / 5, 5, height / 6, 2); 
+    // enemy4=createSprite(width / 5, 5, height / 6, 2); 
+
 }
 
+
+
 function draw() {
-    // setInterval(createNewSprites(), 300000);
-    
+    timeCounter()
+    console.log(time)
     if (isGameOver) {
         gameOver()
     }
     else {
-    if (enemy.overlap(player) || enemy2.overlap(player) || enemy3.overlap(player)) {
-        isGameOver = true
-    }
-    
+    // if (enemy.overlap(player) || enemy2.overlap(player) || enemy3.overlap(player) || createEnemy.overlap(player)) {
+    //     isGameOver = true
+    // }
+
     if (keyDown(RIGHT_ARROW) && player.position.x < width - 30) {
         player.position.x = player.position.x + 10
     }
@@ -49,8 +56,9 @@ function draw() {
     enemy.position.y = enemy.position.y + 6;
     enemy2.position.y = enemy2.position.y + 4;
     enemy3.position.y = enemy3.position.y + 8;
+    enemy4.position.y = enemy4.position.y += random(4,9)
     
-    if (enemy.position.y > height + 100 && enemy2.position.y > height + 100 && enemy3.position.y > 100) {
+    if (enemy.position.y > height + 100 && enemy2.position.y > height + 100 && enemy3.position.y > 100 && enemy4.position.y > 100) {
         incrementScore();
         // console.log(score);
         enemy.position.y = 0
@@ -59,11 +67,16 @@ function draw() {
         enemy2.position.x = random(10, width - 5)
         enemy3.position.y = 0
         enemy3.position.x = random(10, width - 5)
+        enemy4.position.y = 0
+        enemy4.position.x = random(10, width - 5)
+    } if(score===500){
+        enemy5 = createSprite(width/5,3, height/6,2);
     }
 }
     background(33,33,33);
+    clear()
     drawSprites();
-}
+};
 
 function restart(){
     if(isGameOver){
@@ -80,7 +93,6 @@ function restart(){
 }
 
 function gameOver() {
-    // restart()
     modal.classList.add('active');
     modal.appendChild(ptag);
     modal.appendChild(btn);   
@@ -93,8 +105,6 @@ function incrementScore() {
     score += 50
     scoreBoard.innerText = score;
 }
-function createNewSprites(){
-    newSprite = setInterval(createSprite(random(width),random(height)),3000);
-    console.log(newSprite);
+function timeCounter(){
+    return setInterval(time+=1, 3000);
 }
-
