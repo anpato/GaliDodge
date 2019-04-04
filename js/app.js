132066f8f0b2212d486c6f69366e42650766100b
Andre Pato <anpato1994@gmail.com>
2019-04-04 13:27:41 -0400

    dynamically add walls and uncommented audio

--- js/app.js
+++ js/app.js
@@ -6,13 +6,10 @@ let ptag = document.createElement('p');
 //P5 Methods
 let player
 let playerImage
-let enemy
-let score = 0;
 let isGameOver;
 let time = 0;
-
+let score = 0;
 let wallsArr = [];
-let newShape;
 
 
 function preload() {
@@ -28,15 +25,14 @@ function setup() {
     for (let i = 0; i < 3; i++) {
         wallsArr.push(new Walls())
     }
-    // setInterval(() => {
-    //     for (let i = 0; i < 3; i++) {
-    //         wallsArr.push(new Walls())
-    //     }
-    // }, 6000);
-    // console.log(player.size);
-    
+    setInterval(()=> {
+        if(score > 500){
+            for(let i=0; i< 2; i++){
+                wallsArr.push(new Walls);
+            }
+        }
+    }, 5000)
 }
-
 
 function draw() {
     background(33, 33, 33);
@@ -46,12 +42,8 @@ function draw() {
         wallsArr[i].move();
         wallsArr[i].display();
         wallsArr[i].checkCollision();
+        // wallsArr[i].create();
         wallsArr[i].reset();
-        // console.log(Math.floor(wallsArr[i].position.x));
-        // console.log(Math.floor(wallsArr[i].position.y));
-        //  console.log(Math.floor(wallsArr[i].w));
-        
-        // console.log(obstacle);
     }
     if (isGameOver) {
         gameOver()
@@ -62,8 +54,8 @@ function draw() {
         if (keyDown(LEFT_ARROW) && player.position.x > 30) {
             player.position.x = player.position.x - 10
         }
-        // if (obstacle.position.y > height + 100) {}
     }
+    console.log(wallsArr.length)
 };
 
 function restart() {
@@ -92,43 +84,40 @@ function incrementScore() {
     scoreBoard.innerText = score;
 }
 class Walls {
-    constructor(x,y) {
+    constructor() {
         this.x = random(width);
         this.y = random(height);
         this.w = random(width / 20, 180)
         this.h = random(height / 50);
-        this.speed = random(1,3);
+        this.speed = random(1, 3);
     }
-
     move() {
         this.x += random(-this.speed, this.speed);
         this.y += random(+this.speed, this.speed);
         if (this.y > height + 100) {
             this.y = 0;
             incrementScore();
-            // console.log(score);
         }
     }
-    reset(){
-        isGameOver === true ? this.y=0 : null
+    reset() {
+        isGameOver === true ? this.y = 0 : null
     }
     display() {
         rect(this.x, this.y, this.w, this.h);
-        
+        fill('#4dd0e1')
     }
-    checkCollision(){
-        let rectX = Math.floor(this.x);
+
+    checkCollision() {
+        let rectX = this.x;
         let rectY = Math.floor(this.y);
         let rectW = Math.floor(this.w);
         let rectH = Math.floor(this.h);
-        let playerCenter = player.position.x / player.position.y
         let playerX = player.position.x;
         let playerY = player.position.y;
-        if(playerX > rectX && playerX < rectX+rectW){
-            if(player.position.y > rectY && playerY < rectY+rectH){
+        if (playerX > rectX && playerX < rectX + rectW) {
+            if (player.position.y > rectY && playerY < rectY + rectH) {
                 isGameOver = true;
             }
         }
     }
 }
-
